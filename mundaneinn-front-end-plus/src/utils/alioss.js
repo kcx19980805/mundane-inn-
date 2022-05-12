b640d58194bbd14c2bd83f201e24fddc14ca3dc4
kangchenxi <1255753142@qq.com>
2022-05-12 19:10:43 +0800

    update

--- mundaneinn-front-end-plus/src/utils/alioss.js
+++ mundaneinn-front-end-plus/src/utils/alioss.js
@@ -2,9 +2,9 @@ var OSS = require('ali-oss');
 export function client() {
     var client = new OSS({
         region: 'oss-cn-shenzhen',
-        accessKeyId: 'LTAI4GJ71dyqDAvPpVJG7FY6',
-        accessKeySecret: 'IMRqUyUsokGMKud3KUVl7M2LdQ8fqI',
-        bucket: 'kcxbucket'
+        accessKeyId: '...',
+        accessKeySecret: '...',
+        bucket: '...'
     })
     return client
 }
